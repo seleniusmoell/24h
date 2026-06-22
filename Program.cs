49a19dd450314ea6394d418c6336f17c4776ead1
seleniusmoell <seleniusmoell@gmail.com>
2026-06-22 21:50:54 +0200

    First draft

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using _24hKlimatet.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<_24hKlimatet.Services.ScheduleService>();
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseStatusCodePagesWithReExecute("/not-found");
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
